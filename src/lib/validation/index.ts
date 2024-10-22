540e37d43edae31b8e5c267ae002d0e593b8f889
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-22 22:16:38 +0545

    form validation with z resolver

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod"
+
+export const signUpValidationSchema = z.object({
+    name:z.string().min(2,{message:'The name cannot be this short'}).max(16),
+    username: z.string().min(6,{message:'Choose a longer username'}).max(50),
+    email:z.string().email({message:"Please provide a valid email address!"}),
+    password:z.string().min(8,{message:"Password should be of minimum 8 characters"})
+  })
