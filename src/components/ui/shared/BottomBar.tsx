617b4222732d85d5127eb995b5c06ce7798c3d8e
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-23 16:17:45 +0545

    add ui components

---
+++ src/components/ui/shared/BottomBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BottomBar = () => {
+  return (
+    <div>
+      bottombar
+    </div>
+  )
+}
+
+export default BottomBar
---
+++ src/components/ui/shared/TopBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '../button'
+
+const TopBar = () => {
+  return (
+    <section className='topbar'>
+        <div className='flex-between py-4 px-5'>
+            <Link to="/" className="flex gap-3 items-center">
+                <img src="/assets/images/logo.svg" alt="" width={130} height={325}/>
+            </Link>
+            <div className='flex gap-4'>
+                <Button variant="ghost" className='shad-button_ghost'
+                    
+                >
+                    <img src="/assets/icons/logout.svg" alt="logout" />
+                </Button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default TopBar
