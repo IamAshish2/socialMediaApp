e2532ac4d160c4342ce24c6770752b9c5adffad9
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-23 16:16:30 +0545

    home page layout section defined

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import BottomBar from '@/components/ui/shared/BottomBar'
+import LeftSidebar from '@/components/ui/shared/LeftSidebar'
+import TopBar from '@/components/ui/shared/TopBar'
+import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
   return (
-    <div>
-      
+    <div className='w-full md:flex'>
+        <TopBar/>
+        <LeftSidebar/>
+
+        <section className='flex flex-1 h-full'>
+          <Outlet/>
+        </section>
+        <BottomBar/>
     </div>
   )
 }
