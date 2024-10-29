ba7f7c3f90818c1556cc1fcfe71da8a4a8fa9be8
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-29 21:19:21 +0545

    define types properly

--- src/types/index.ts
+++ src/types/index.ts
@@ -36,7 +36,7 @@ export type INavLink = {
     postId: string;
     caption: string;
     imageId: string;
-    imageUrl: URL;
+    imageUrl:  URL | string; // URL | string
     file: File[];
     location?: string;
     tags?: string;
